265101c697c137573c59a4b41ea3d463c026deb5
mozi1924 <mozi1924@arasaka.ltd>
2026-08-26 00:13:37 +0800

    chore: add ArrayList and HashMap imports to WebSocketServerManager

--- src/main/java/com/mozi1924/yefira/network/WebSocketServerManager.java
+++ src/main/java/com/mozi1924/yefira/network/WebSocketServerManager.java
@@ -12,7 +12,9 @@ import org.java_websocket.server.WebSocketServer;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
